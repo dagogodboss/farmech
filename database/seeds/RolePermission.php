2aef7d7ca9a630c6529e8001bffb82467b86793e
Ilamini Dagogo <dagogodboss@gmail.com>
2020-10-29 18:02:07 +0100

    Seeder Set

--- database/seeds/RolePermission.php
+++ database/seeds/RolePermission.php
@@ -42,31 +42,6 @@ class RolePermission extends Seeder
             ],
         ]);
 
-        DB::table('role_permissions')->insert([
-            [
-                'role_id' => 2,
-                'permission_id' => 1
-            ],
-            [
-                'role_id' => 2,
-                'permission_id' => 2
-            ],
-            [
-                'role_id' => 2,
-                'permission_id' => 3
-            ],
-            [
-                'role_id' => 2,
-                'permission_id' => 4
-            ],
-            [
-                'role_id' => 1,
-                'permission_id' => 3
-            ],
-            [
-                'role_id' => 1,
-                'permission_id' => 4
-            ],
-        ]);
+
     }
 }
